24e21aecb1748d3322fba8d414138318b759d35c
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-09-26 17:10:59 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,7 +28,6 @@ int main() {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
-
     testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
     testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
 
