76a9fb37879018855893ebced1f192f9e1f0c11f
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-09-26 16:49:44 +0530

    Create ColorCoder.h

---
+++ ColorCoder.h
@@ -0,0 +1,10 @@
+#include "ColorPair.h"
+
+namespace TelCoColorCoder 
+{
+    ColorPair GetColorFromPairNumber(int pairNumber);
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+    void PrintColorReferenceManual();
+}
+
+#endif
