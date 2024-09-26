84c48c07401bd5438f851a8a1ad13f71ad9a3281
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-09-26 16:48:00 +0530

    Create ColorCoder.cpp

---
+++ ColorCoder.cpp
@@ -0,0 +1,19 @@
+#include "ColorCoderUtils.h"
+#include <iostream>
+
+namespace TelCoColorCoder 
+{
+    int MajorColorsNos = 5;
+    int MinorColorNos = 5;
+
+    ColorPair GetColorFromPairNumber(int pairNumber) {
+        int zeroBasedPairNumber = pairNumber - 1;
+        MajorColor majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
+        MinorColor minorColor = (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
+        return ColorPair(majorColor, minorColor);
+    }
+
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+        return major * numberOfMinorColors + minor + 1;
+    }
+}
