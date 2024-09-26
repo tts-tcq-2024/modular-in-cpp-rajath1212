8f3845acd403a5e9af934129ba196478a4e0a341
rajath1212 <80102711+rajath1212@users.noreply.github.com>
2024-09-26 16:56:29 +0530

    Create pairing.h

---
+++ pairing.h
@@ -0,0 +1,23 @@
+#ifndef PAIRING_H
+#define PAIRING_H
+
+#include <string>
+
+namespace TelCoColorCoder 
+{
+    enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+    enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+    class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor);
+            MajorColor getMajor();
+            MinorColor getMinor();
+            std::string ToString();
+    };
+}
+
+#endif
